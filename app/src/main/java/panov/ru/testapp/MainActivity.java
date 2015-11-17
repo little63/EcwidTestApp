eb5da108ba19cf274a0c77629d371b4e753261d4
Vitaly Panov <vetalpanov@gmail.com>
2015-11-18 01:38:29 +0300

    connect ProductItemAdapter with main activity.

--- app/src/main/java/panov/ru/testapp/MainActivity.java
+++ app/src/main/java/panov/ru/testapp/MainActivity.java
@@ -2,6 +2,8 @@ package panov.ru.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -13,11 +15,28 @@ import panov.ru.testapp.model.ProductItem;
 public class MainActivity extends AppCompatActivity {
 
     private List<ProductItem> productItems = new ArrayList<ProductItem>();
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new ProductItemAdapter(productItems);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     @Override
--- app/src/main/java/panov/ru/testapp/ProductItemAdapter.java
+++ app/src/main/java/panov/ru/testapp/ProductItemAdapter.java
@@ -1,17 +1,22 @@
 package panov.ru.testapp;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import panov.ru.testapp.model.ProductItem;
 
 /**
  * Created by vitaly.panov on 18.11.15.
  */
 public class ProductItemAdapter extends RecyclerView.Adapter<ProductItemAdapter.ViewHolder> {
 
-    private ArrayList<String> dataset;
+    private List<ProductItem> dataset;
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
@@ -25,16 +30,54 @@ public class ProductItemAdapter extends RecyclerView.Adapter<ProductItemAdapter.
         }
     }
 
-    public void add(int position, String item) {
+    public void add(int position, ProductItem item) {
         dataset.add(position, item);
         notifyItemInserted(position);
     }
 
-    public void remove(String item) {
+    public void remove(ProductItem item) {
         int position = dataset.indexOf(item);
         dataset.remove(position);
         notifyItemRemoved(position);
     }
 
-    
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public ProductItemAdapter(List<ProductItem> myDataset) {
+        dataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ProductItemAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_list_item, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        /*final String name = dataset.get(position);
+        holder.txtHeader.setText(dataset.get(position));
+        holder.txtHeader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                remove(name);
+            }
+        });
+
+        holder.txtFooter.setText("Footer: " + dataset.get(position));*/
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return dataset.size();
+    }
 }
