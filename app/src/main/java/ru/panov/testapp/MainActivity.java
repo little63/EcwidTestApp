6d4b806cbd80d4091a3499f9d9981e846215ed07
Vitaly Panov <vetalpanov@gmail.com>
2015-11-25 10:53:04 +0300

    connect androidannotations framework.

--- app/src/main/java/ru/panov/testapp/MainActivity.java
+++ app/src/main/java/ru/panov/testapp/MainActivity.java
@@ -2,16 +2,20 @@ package ru.panov.testapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import org.androidannotations.annotations.EActivity;
+
 import ru.panov.testapp.model.ProductItem;
 import ru.panov.testapp.products.DetailActivity;
 import ru.panov.testapp.products.RecyclerViewFragment;
 
+@EActivity(R.layout.activity_main)
 public class MainActivity extends BaseActivity implements RecyclerViewFragment.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
         //hide action bar
         getSupportActionBar().hide();
